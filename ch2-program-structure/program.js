5a615b3bf62971a32d76f2250c9154742a8d0609
Simon <simonxd@gmail.com>
2015-03-17 17:43:03 -0300

    basic program

---
+++ ch2-program-structure/program.js
@@ -0,0 +1,13 @@
+var separador = '=================================================';
+
+var a = 5;
+var b = 7;
+var resultado = a + b;
+console.log(resultado);
+console.log(separador);
+
+
+var c = 15, d = 77, resultado2 = c + d;
+console.log(resultado2);
+console.log(separador);
+
